2a848da43c5db446070dd64d43849c5fe29100b9
MartinNoboa <nomonoboa@gmail.com>
2021-06-01 22:02:39 -0500

    Actualizar repo

---
+++ Swift/CountriesApp/AddCountryView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  CountriesApp
+//
+//  Created by user190124 on 13/5/21.
+//
+
+import SwiftUI
+
+struct AddCountryView: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    @ObservedObject var countryController: Countrycontroller
+    
+    @State var countryName: String = ""
+    @State var population: String = ""
+    
+    
+    var body: some View {
+        VStack{
+            Text("Please Add a new coutry with the population")
+                .font(.title2)
+                .multilineTextAlignment(.center)
+            .padding(.bottom, 30)
+            
+            HStack{
+                Image(systemName: "pencil.circle")
+                    .foregroundColor(.gray)
+                TextField("Country", text: $countryName)
+            }
+            .padding()
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 1.0))
+            
+            HStack{
+                Image(systemName: "person.3")
+                    .foregroundColor(.gray)
+                TextField("Population", text: $population)
+            }
+            .padding()
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 1.0))
+            
+            HStack {
+                
+                //MARK: CANCEL BUTTON
+                Button("Cancel") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+                .padding(.all)
+                .padding(.horizontal)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(7.0)
+                
+                
+                //MARK: Submit BUTTON
+                Button(action: {
+                    countryController.addCountry(newCountry: CountryModel(id: UUID(), name: countryName, population: population))
+                    
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("Add Country")
+                }
+                .padding(.all)
+
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(7.0)
+            }
+            .padding(.top, 30)
+
+        }// VStack
+        .padding(.horizontal, 10)
+        .navigationBarTitle("Add a New Country")
+        .navigationBarBackButtonHidden(true)
+        
+    }
+}
+
+struct AddCountryView_Previews: PreviewProvider {
+    
+    //static var controller = Countrycontroller()
+
+    
+    static var previews: some View {
+        AddCountryView(countryController: Countrycontroller())
+    }
+}
+
--- Swift/CountriesApp/ContentView.swift
+++ Swift/CountriesApp/ContentView.swift
@@ -9,20 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var countryController = Countrycontroller()
+    @ObservedObject var countryController = Countrycontroller()
+   
     
     var body: some View {
         NavigationView {
-            List(countryController.countries) { country in
-                Text(country.name)
-            } 
+            List {
+                ForEach(countryController.countries){ country in
+                    NavigationLink(destination: DetailView(country: country)) {
+                        Text(country.name)
+                    }
+                }.onDelete(perform: deleteItem)
+            }
+            
             .navigationBarTitle("Countries", displayMode: .inline)
-        }         
+
+            .navigationBarItems(
+                trailing:
+                    NavigationLink(
+                        destination: AddCountryView(countryController: countryController)) {
+                    Image(systemName: "plus").foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                })
+        } // NavigationView
     }
+    
+    
+    private func deleteItem(at indexSet: IndexSet) {
+        self.countryController.countries.remove(atOffsets: indexSet)
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+          ContentView()
     }
 }
